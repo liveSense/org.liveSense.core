2138b74903097d9b9b73b7a76d86a267bbd77cd7
Robert Csakany <csakany.robert@gmail.com>
2012-11-16 20:17:15 +0100

    SessionFactoryImpl and SessionFactoryService merged

--- src/main/java/org/liveSense/core/session/SessionFactoryImpl.java
+++ src/main/java/org/liveSense/core/session/SessionFactoryImpl.java
@@ -12,36 +12,77 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Component(immediate=true, metatype=true)
+@Service(SessionFactory.class)
+@Properties(value={
+		@Property(name=SessionFactoryImpl.PROP_SESSION_TIMEOUT_CHECK_INTERVAL, longValue={SessionFactoryImpl.DEFAULT_SESSION_TIMEOUT_CHECK_INTERVAL}),
+		@Property(name=SessionFactoryImpl.PROP_DEFAULT_SESSION_TIMEOUT, longValue={SessionFactoryImpl.DEFAULT_DEFAULT_SESSION_TIMEOUT}),
+		@Property(name=SessionFactoryImpl.PROP_CLOSE_TASK_TIMEOUT, longValue={SessionFactoryImpl.DEFAULT_CLOSE_TASK_TIMEOUT})
+})
 
 public class SessionFactoryImpl implements SessionFactory {
-	
+
+	public final static String PROP_SESSION_TIMEOUT_CHECK_INTERVAL = "sessionTimeoutCheckInterval";
+	public final static long DEFAULT_SESSION_TIMEOUT_CHECK_INTERVAL = 500;
+
+	public final static String PROP_CLOSE_TASK_TIMEOUT = "closeTaskTimeout";
+	public final static long DEFAULT_CLOSE_TASK_TIMEOUT = 60 * 1000;
+
+	public final static String PROP_DEFAULT_SESSION_TIMEOUT = "defaultSessionTimeout";
+	public final static long DEFAULT_DEFAULT_SESSION_TIMEOUT = 30 * 1000;
+
 	static final Logger log = LoggerFactory.getLogger(SessionFactoryImpl.class);
-	
+
 	private final Map<String, Session> simpleSessionTracker = new ConcurrentHashMap<String, Session>();
 	private ExecutorService closeTaskExecuter;
 	private ScheduledExecutorService cleaningExecuter;
-	
-	long sessionTimeoutCheckInterval = 500;  // 500 ms
-	long closeTaskTimeout = 60 * 1000;       // 1 minutes
-	long defaultSessionTimeout = 30 * 1000; // 30 sec
-	
+
+	long sessionTimeoutCheckInterval = DEFAULT_SESSION_TIMEOUT_CHECK_INTERVAL;
+	long closeTaskTimeout = DEFAULT_CLOSE_TASK_TIMEOUT;
+	long defaultSessionTimeout = DEFAULT_DEFAULT_SESSION_TIMEOUT;
+
 	private Runnable checkAndRemoveTimeOutedSession;
-	
+
+	private static SessionFactoryImpl INSTANCE = null;
+
+	@Activate
+	protected void activate(ComponentContext context) {
+		closeTaskTimeout = PropertiesUtil.toLong(context.getProperties().get(PROP_CLOSE_TASK_TIMEOUT), DEFAULT_CLOSE_TASK_TIMEOUT);
+		sessionTimeoutCheckInterval = PropertiesUtil.toLong(context.getProperties().get(PROP_SESSION_TIMEOUT_CHECK_INTERVAL), DEFAULT_SESSION_TIMEOUT_CHECK_INTERVAL);
+		defaultSessionTimeout = PropertiesUtil.toLong(context.getProperties().get(PROP_DEFAULT_SESSION_TIMEOUT), DEFAULT_DEFAULT_SESSION_TIMEOUT);
+		log.info("Creating liveSense Session Factory from OSGi. \n   Default session timeout: {}\n   Session timeout check interval: {}\n   Session close timeout: {}\n    Factory close timeout: {} ", new Object[]{INSTANCE.defaultSessionTimeout, INSTANCE.sessionTimeoutCheckInterval, INSTANCE.closeTaskTimeout});
+		createExecuters();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		close();
+	}
+
 	private void createExecuters() {
 		closeTaskExecuter = Executors.newCachedThreadPool();
 		//simpleSessionTracker = new HashMap<String, Session>();
 		cleaningExecuter = Executors.newSingleThreadScheduledExecutor();
-			
+
 		checkAndRemoveTimeOutedSession = new Runnable() {
 
+			@Override
 			public void run() {
 
 				if (log.isDebugEnabled())
 					log.info("Running session timeout checker");
-				
+
 				for (Entry<String, Session> session : simpleSessionTracker.entrySet()) {
 					// Refresing session
 					session.getValue().validate();
@@ -56,16 +97,17 @@ public class SessionFactoryImpl implements SessionFactory {
 				}
 			}
 		};
-		
+
 		cleaningExecuter.scheduleAtFixedRate(checkAndRemoveTimeOutedSession, 0, sessionTimeoutCheckInterval, TimeUnit.MILLISECONDS);
-	
+
 	}
-	
+
+	@Override
 	public Session getSession(String sessionId) {
 
 		Session session = null;
 		session = simpleSessionTracker.get(sessionId.toString());
-			
+
 		if (session != null) {
 			updateSession(session);
 		} else {
@@ -74,16 +116,19 @@ public class SessionFactoryImpl implements SessionFactory {
 		return session;
 	}
 
+	@Override
 	public void updateSession(Session session) {
 		if (session == null) return;
 		session.refresh();
 	}
 
+	@Override
 	public void updateSession(String sessionId) {
 		updateSession(getSession(sessionId));
 	}
 
-	
+
+	@Override
 	public void removeSession(Session session) {
 		if (session == null) return;
 
@@ -93,17 +138,17 @@ public class SessionFactoryImpl implements SessionFactory {
 			if (log.isDebugEnabled())
 				log.debug("Executing close() in single thread on session: "+session.getId());
 			closeTaskExecuter.invokeAll(Arrays.asList(
-				new SessionCloseTask(new WeakReference<SessionFactory>(this), session) {
-					
-					@Override
-					public void onClose(WeakReference<SessionFactory> factory, Session session) {
-						
-						session.close();
-						if (log.isDebugEnabled())
-							log.debug("Session closed successfully: "+session.getId());
-					}
-				}),
-			closeTaskTimeout, TimeUnit.MILLISECONDS);
+					new SessionCloseTask(new WeakReference<SessionFactory>(this), session) {
+
+						@Override
+						public void onClose(WeakReference<SessionFactory> factory, Session session) {
+
+							session.close();
+							if (log.isDebugEnabled())
+								log.debug("Session closed successfully: "+session.getId());
+						}
+					}),
+					closeTaskTimeout, TimeUnit.MILLISECONDS);
 		} catch (InterruptedException e) {	
 			log.error("Could not complete close of the session: "+session.getId());
 		}
@@ -112,21 +157,24 @@ public class SessionFactoryImpl implements SessionFactory {
 		log.info("Session removed: "+session.getId().toString());
 		simpleSessionTracker.remove(session.getId().toString());
 	}
-	 
+
+	@Override
 	public void removeSession(String sessionId) {
 		removeSession(getSession(sessionId));
 	}
-	
+
 	public boolean isClosed(Session session) {
 		if (session == null) return false;
 		return session.isClosed();
 	}
 
-		
+
+	@Override
 	public Session createDefaultSession() throws Throwable {
 		return createSession(SessionImpl.class);
 	}
-	
+
+	@Override
 	public Session createSession(@SuppressWarnings("rawtypes") Class clazz) throws Throwable {
 		return createSession(null, clazz, null);
 	}
@@ -148,7 +196,7 @@ public class SessionFactoryImpl implements SessionFactory {
 			if (clazz == null) 
 				throw new Exception("Class does not defined for SessionFactory");
 		}
-		
+
 		Session ret = null;
 		try {
 			Class[] constructorArgsClass = new Class[] {SessionFactory.class};
@@ -157,7 +205,7 @@ public class SessionFactoryImpl implements SessionFactory {
 			if (constructor == null) {
 				throw new Exception("Constructor with SessionFactory parameter not found for class: "+clazz.getName());
 			}
-			
+
 			ret = (Session)constructor.newInstance(constructorArgs);
 			ret.setTimeout(defaultSessionTimeout);
 			log.info("Session created: "+ret.getId().toString());
@@ -168,36 +216,41 @@ public class SessionFactoryImpl implements SessionFactory {
 		}
 		return ret;
 	}
-	
-	private static SessionFactoryImpl factory = null;
-	private SessionFactoryImpl() {
-	}
-	
-	public static SessionFactory getInstance(long defaultSessionTimeout, long sessionTimeoutCheckInterval, long sessionCloseTimeout) {
-		if (factory == null) {
-			log.info("Creating liveSense Session Factory. \n   Default session timeout: {}\n   Session timeout check interval: {}\n   Session close timeout: {}\n    Factory close timeout: {} ", new Object[]{defaultSessionTimeout, sessionTimeoutCheckInterval, sessionCloseTimeout});
-			factory = new SessionFactoryImpl();
-			factory.sessionTimeoutCheckInterval = sessionTimeoutCheckInterval;
-			factory.closeTaskTimeout = sessionCloseTimeout;
-			factory.defaultSessionTimeout = defaultSessionTimeout;
-			factory.createExecuters();
+
+	public SessionFactoryImpl() {
+		if (INSTANCE != null) {
+			INSTANCE.close();
 		}
-		return factory;
+		INSTANCE = this;
+		INSTANCE.createExecuters();
+	}
+
+	public static SessionFactory getInstance(long defaultSessionTimeout, long sessionTimeoutCheckInterval, long sessionCloseTimeout) {
+		if (INSTANCE == null) {
+			log.debug("Creating liveSense Session Factory. \n   Default session timeout: {}\n   Session timeout check interval: {}\n   Session close timeout: {}\n    Factory close timeout: {} ", new Object[]{defaultSessionTimeout, sessionTimeoutCheckInterval, sessionCloseTimeout});
+			INSTANCE = new SessionFactoryImpl();
+			INSTANCE.sessionTimeoutCheckInterval = sessionTimeoutCheckInterval;
+			INSTANCE.closeTaskTimeout = sessionCloseTimeout;
+			INSTANCE.defaultSessionTimeout = defaultSessionTimeout;
+			INSTANCE.createExecuters();
+		}
+		return INSTANCE;
 	}
 
 	public static SessionFactory getInstance() {
-		if (factory == null) {
-			factory = new SessionFactoryImpl();
-			log.info("Creating liveSense Session Factory. \n   Default session timeout: {}\n   Session timeout check interval: {}\n   Session close timeout: {}\n    Factory close timeout: {} ", new Object[]{factory.defaultSessionTimeout, factory.sessionTimeoutCheckInterval, factory.closeTaskTimeout});
-			factory.createExecuters();
+		if (INSTANCE == null) {
+			INSTANCE = new SessionFactoryImpl();
+			log.debug("Creating liveSense Session Factory. \n   Default session timeout: {}\n   Session timeout check interval: {}\n   Session close timeout: {}\n    Factory close timeout: {} ", new Object[]{INSTANCE.defaultSessionTimeout, INSTANCE.sessionTimeoutCheckInterval, INSTANCE.closeTaskTimeout});
+			INSTANCE.createExecuters();
 		}
-		return factory;
+		return INSTANCE;
 	}
 
+	@Override
 	public void close() {
-			cleaningExecuter.shutdown();
-			closeTaskExecuter.shutdown();
-			factory = null;
+		cleaningExecuter.shutdown();
+		closeTaskExecuter.shutdown();
+		INSTANCE = null;
 	}
-	
+
 }
